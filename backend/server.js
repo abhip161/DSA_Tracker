e30f307c07fd2fb48998eefbb45e942b58ffb125
Abhishek Patel <Abhi161511@gmail.com>
2026-02-26 20:38:52 +0530

    Modify CORS settings in server.js
    Updated CORS configuration to allow specific origin.

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://dsa-tracker-gules.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 
 app.get('/api/health', (req, res) => {
